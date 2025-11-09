4217d015fb8ba47d0f8c08d759c9e1c31cb04e42
Kartik R K <kartikrk805@gmail.com>
2025-11-09 22:45:26 +0530

    Add files via upload

---
+++ smaller or greater.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+   printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    if (num1 > num2) {
+        printf("%d is greater than %d\n", num1, num2);
+    }
+    else if (num1 < num2) {
+        printf("%d is smaller than %d\n", num1, num2);
+    }
+    else {
+        printf("Both numbers are equal\n");
+    }
+
+    return 0;
+}
