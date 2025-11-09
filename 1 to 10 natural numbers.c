c58b866594cb7887ed425481e81dc76af3fe66af
Kartik R K <kartikrk805@gmail.com>
2025-11-09 22:54:45 +0530

    Refactor to use while loop for printing numbers

--- 1 to 10 natural numbers.c
+++ 1 to 10 natural numbers.c
@@ -1,10 +1,13 @@
-#include<stdio.h>
-int main()
-{
-    int i;
-    for(i=1;i<11;++i)
-    {
-    printf("%d",i);
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+    while(i <= 10) {
+        printf("%d\n", i);
+        i++;
     }
+
     return 0;
 }
+
